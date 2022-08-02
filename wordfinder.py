b59931a37b001f71f3c36646e4c337993bfc71b0
dqoborsi <dqoborsi@gmail.com>
2022-08-02 13:42:05 -0700

    wordfinder working

--- wordfinder.py
+++ wordfinder.py
@@ -1,15 +1,17 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
     ...
     def __init__(self, words_file):
         ...
+        text = open(words_file)
+        self.list_of_words = [word.strip() for word in text]
 
 
     def random(self):
         ...
-        f = open("words.txt")
-        f.read()
+        return random.choice(self.list_of_words)
 
-
+test = WordFinder('words.txt')
+print(test.random())
